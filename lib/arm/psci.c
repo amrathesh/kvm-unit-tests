7b37507f6bacbf902197432a67be3a099f04a3d5
Nikos Nikoleris <nikos.nikoleris@arm.com>
2022-05-01 19:12:45 +0100

    arm/arm64: Add support for setting up the PSCI conduit through ACPI
    In systems with ACPI support and when a DT is not provided, we can use
    the FADT to discover whether PSCI calls need to use smc or hvc
    calls. This change implements this but retains the default behavior;
    we check if a valid DT is provided, if not, we try to setup the PSCI
    conduit using ACPI.
    
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

--- lib/arm/psci.c
+++ lib/arm/psci.c
@@ -6,6 +6,7 @@
  *
  * This work is licensed under the terms of the GNU LGPL, version 2.
  */
+#include <acpi.h>
 #include <devicetree.h>
 #include <asm/psci.h>
 #include <asm/setup.h>
@@ -56,7 +57,7 @@ void psci_system_off(void)
 	printf("CPU%d unable to do system off (error = %d)\n", smp_processor_id(), err);
 }
 
-void psci_set_conduit(void)
+static void psci_set_conduit_fdt(void)
 {
 	const void *fdt = dt_fdt();
 	const struct fdt_property *method;
@@ -74,4 +75,24 @@ void psci_set_conduit(void)
 		psci_invoke = psci_invoke_smc;
 	else
 		assert_msg(false, "Unknown PSCI conduit: %s", method->data);
+}
+
+static void psci_set_conduit_acpi(void)
+{
+	struct acpi_table_fadt *fadt = find_acpi_table_addr(FACP_SIGNATURE);
+	assert_msg(fadt, "Unable to find ACPI FADT");
+	assert_msg(fadt->arm_boot_flags & ACPI_FADT_PSCI_COMPLIANT,
+		   "PSCI is not supported in this platfrom");
+	if (fadt->arm_boot_flags & ACPI_FADT_PSCI_USE_HVC)
+		psci_invoke = psci_invoke_hvc;
+	else
+		psci_invoke = psci_invoke_smc;
+}
+
+void psci_set_conduit(void)
+{
+	if (dt_available())
+		psci_set_conduit_fdt();
+	else
+		psci_set_conduit_acpi();
 }
