4d32c710a77de343ca0762ee356c19e72273d3b4
Nikos Nikoleris <nikos.nikoleris@arm.com>
2022-05-03 09:32:18 +0100

    arm/arm64: Add a setup sequence for systems that boot through EFI
    This change implements an alternative setup sequence for the system
    when we are booting through EFI. The memory map is discovered through
    EFI boot services and devices through ACPI.
    
    This change is based on a change initially proposed by
    Andrew Jones <drjones@redhat.com>
    
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

--- lib/arm/asm/setup.h
+++ lib/arm/asm/setup.h
@@ -6,6 +6,7 @@
  * This work is licensed under the terms of the GNU LGPL, version 2.
  */
 #include <libcflat.h>
+#include <efi.h>
 #include <asm/page.h>
 #include <asm/pgtable-hwdef.h>
 
@@ -37,5 +38,6 @@ extern unsigned int mem_region_get_flags(phys_addr_t paddr);
 #define SMP_CACHE_BYTES		L1_CACHE_BYTES
 
 void setup(const void *fdt, phys_addr_t freemem_start);
+efi_status_t setup_efi(efi_bootinfo_t *efi_bootinfo);
 
 #endif /* _ASMARM_SETUP_H_ */
